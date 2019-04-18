6f5f5f9a4d030a7aaf40c998716d8d31844b9b6c
Karthik Kumar <karthik@lightstep.com>
2019-04-17 23:19:15 -0700

    add logo

--- nuggets/settings.py
+++ nuggets/settings.py
@@ -140,6 +140,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+STATICFILES_DIRS = [  # For static files not particular to any app.
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
