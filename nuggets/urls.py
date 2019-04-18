748d783d4a4914ab2d7712fb7b7a47c154652a1a
Karthik Kumar <karthik@lightstep.com>
2019-04-17 22:38:39 -0700

    add login page

--- nuggets/urls.py
+++ nuggets/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^login$', TemplateView.as_view(template_name='login.html')),
     url(r'', include('nuggets_api.urls')),
 ]
